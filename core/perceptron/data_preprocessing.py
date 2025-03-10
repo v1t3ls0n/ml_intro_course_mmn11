a4836c21a4de946383c44da227ec05eaf5c62bc6
v1t3ls0n <guyvitelson@gmail.com>
2025-03-10 15:43:33 +0200

    save work

--- core/perceptron/data_preprocessing.py
+++ core/perceptron/data_preprocessing.py
@@ -1,15 +1,17 @@
-# Data Loading and Preprocessing Functions
+# data_preprocessing.py
+
 import numpy as np
 from sklearn.datasets import fetch_openml
 
 def load_mnist():
     """
-    Loads the MNIST dataset using fetch_openml.
+    Loads the MNIST dataset using fetch_openml, default parser => requires pandas installed.
     Returns:
         X: Array of shape (n_samples, 784) with pixel features.
         y: Array of shape (n_samples,) with digit labels.
     """
-    mnist = fetch_openml('mnist_784', version=1)
+    # This will use pandas internally, so make sure you have `pip install pandas`.
+    mnist = fetch_openml('mnist_784', version=1)  # default => requires pandas
     X = mnist.data.astype(np.float32)
     y = mnist.target.astype(np.int32)
     return X, y
@@ -17,10 +19,8 @@ def load_mnist():
 def preprocess_data(X):
     """
     Normalizes pixel values to [0, 1] and adds a bias term.
-    Each image (28x28 pixels) is flattened into a 785-dimensional vector,
-    where the first element is set to 1 (bias).
+    Each image (28x28) is flattened into a 785D vector, with first element = 1 (bias).
     """
     X = X / 255.0
-    # Add bias column (first column set to 1)
     X_bias = np.hstack((np.ones((X.shape[0], 1)), X))
     return X_bias
