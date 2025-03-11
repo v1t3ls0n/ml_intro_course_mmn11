e390b512aa82932f64181af4dc52af8a3c3ef161
v1t3ls0n <guyvitelson@gmail.com>
2025-03-11 03:27:13 +0200

    save work

--- core/performance_analysis/perceptron/perceptron_evaluation_functions.py
+++ core/performance_analysis/perceptron/perceptron_evaluation_functions.py
@@ -6,20 +6,22 @@ from sklearn.metrics import confusion_matrix
 from logger.config import logger
 
 
+
 def custom_confusion_matrix(y_true, y_pred, num_classes=10):
-    """
-    Computes the confusion matrix from scratch.
+    '''
+    Computes confusion matrix from scratch.
     Args:
-        y_true: True labels.
-        y_pred: Predicted labels.
-        num_classes: Number of classes (default 10 for MNIST).
+        y_true (array): True labels
+        y_pred (array): Predicted labels
+        num_classes (int): Number of classes
     Returns:
-        A (num_classes x num_classes) confusion matrix.
-    """
+        ndarray: Confusion matrix of shape (num_classes, num_classes)
+    '''
     cm = np.zeros((num_classes, num_classes), dtype=int)
-    for t, p in zip(y_true, y_pred):
-        cm[t, p] += 1
+    for true_label, pred_label in zip(y_true, y_pred):
+        cm[true_label, pred_label] += 1
     return cm
+
 
 def evaluate_model(model, X, y):
     """
